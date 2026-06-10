1c6a730069c526557623bd4da5b7a82882911f5c
mahln908 <mazinholn18@gmail.com>
2026-06-10 19:25:50 -0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'yt-music-v2';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    console.log('Service Worker instalando...');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache aberto');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(response => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    const responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return response;
+                });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker ativado');
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
